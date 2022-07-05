fe3c1f878fe4b0a963dc0326f85c4374ac38f5b8
vanyauhalin <vanyauhalin@gmail.com>
2022-07-05 15:37:54 +0300

    build: add fnm

--- scripts.environment.js
+++ scripts.environment.js
@@ -11,21 +11,25 @@ const DIRNAME = dirname(FILENAME);
 
 async function createDirectory(to) {
   const file = `${DIRNAME}/env/${to}`;
-  const directory = file.replace(`/${basename(file)}`, '');
+  const directory = file.replace(new RegExp(`/${basename(file)}$`), '');
   if (!existsSync(directory)) await mkdir(directory, { recursive: true });
   return file;
 }
 
-function writeCli(line, to) {
+/* eslint-disable promise/prefer-await-to-callbacks */
+function writeCli(line, to, callback) {
   return async () => {
     const [command, ...arguments_] = line.split(' ');
     const process = spawnSync(command, arguments_);
     const error = process.stderr.toString();
     if (error) throw new Error(error);
+    let data = process.stdout.toString();
+    if (callback) data = callback(data);
     const file = await createDirectory(to);
-    await writeFile(file, process.stdout.toString());
+    await writeFile(file, data);
   };
 }
+/* eslint-enable promise/prefer-await-to-callbacks */
 
 function copyLocal(from, to) {
   return async () => {
@@ -45,6 +49,11 @@ script('build-editorconfig', copyLocal(
   '~/.editorconfig',
   'editorconfig/.editorconfig',
 ));
+
+script('build-fnm', writeCli('fnm env', 'fnm/env', (data) => {
+  const [directory] = data.match(/FNM_DIR.*/);
+  return directory || data;
+}));
 
 script('build-git', () => Promise.all([
   script('git-gitconfig', copyLocal('~/.gitconfig', 'git/.gitconfig'))(),
