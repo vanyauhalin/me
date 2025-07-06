f4092ff8c194c133a3ed22046db8244830be9907
vanyauhalin <hi@vanyauhalin.me>
2025-07-06 14:53:24 +0400

    add date properties to posts

--- pages/posts/posts.11tydata.js
+++ pages/posts/posts.11tydata.js
@@ -1,5 +1,29 @@
 export default function() {
 	return {
 		layout: "post.njk",
+		eleventyComputed: {
+			/**
+			 * @param {any} ctx
+			 * @returns {string}
+			 */
+			postDatetime(ctx) {
+				return ctx.page.date.toLocaleDateString("en-CA", {
+					timeZone: "UTC",
+				})
+			},
+
+			/**
+			 * @param {any} ctx
+			 * @returns {string}
+			 */
+			postDisplayDate(ctx) {
+				return ctx.page.date.toLocaleDateString("en-US", {
+					year: "numeric",
+					month: "short",
+					day: "numeric",
+					timeZone: "UTC",
+				})
+			},
+		},
 	}
 }
